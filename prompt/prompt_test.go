62594df257447b48f1186222f57ce2fbbf554a17
Seth Pollen <pollen@google.com>
2014-10-29 17:55:53 -0500

    Unit test

---
+++ prompt/prompt_test.go
@@ -0,0 +1,50 @@
+package prompt
+
+import "strconv"
+import "testing"
+import "time"
+
+var env = PromptEnv{time.Unix(0, 0), "/home/me", "myhost.example.com"}
+
+func assertMakePrompt(t *testing.T, expected string, maxWidth int, info string,
+	pwd string, exitCode int, flag string) {
+	var p = MakePrompt(&env, maxWidth, info, pwd, exitCode, flag)
+	if p.Dump() != expected {
+		t.Errorf("Expected %s\nGot %s",
+			strconv.Quote(expected), strconv.Quote(p.Dump()))
+	}
+}
+
+func TestMakePromptSimple(t *testing.T) {
+	assertMakePrompt(t,
+		"%{\033[0m%}%{\033[1;36m%}12/31 18:00 "+
+			"%{\033[1;35m%}myhost "+
+			"%{\033[1;36m%}/pw/d "+
+			"%{\033[1;33m%}\nflag$ %{\033[0m%}",
+		100, "", "/pw/d", 0, "flag")
+}
+
+func TestMakePromptWithInfoAndExitCode(t *testing.T) {
+	assertMakePrompt(t,
+		"%{\033[0m%}%{\033[1;36m%}12/31 18:00 "+
+			"%{\033[1;35m%}myhost "+
+			"%{\033[0;37m%}[%{\033[1;37m%}info%{\033[0;37m%}] "+
+			"%{\033[1;36m%}/pw/d "+
+			"%{\033[1;31m%}[15]"+
+			"%{\033[1;33m%}\nflag$ %{\033[0m%}",
+		100, "info", "/pw/d", 15, "flag")
+}
+
+func TestMakePromptTruncatedPwd(t *testing.T) {
+	assertMakePrompt(t,
+		"%{\033[0m%}%{\033[1;36m%}12/31 18:00 "+
+			"%{\033[1;35m%}myhost "+
+			"%{\033[0;37m%}[%{\033[1;37m%}info%{\033[0;37m%}] "+
+			"%{\033[1;36m%}..789012345678901234567890 "+
+			"%{\033[1;33m%}\nflag$ %{\033[0m%}",
+		52, "info", "1234567890123456789012345678901234567890", 0, "flag")
+}
+
+func TestMakePromptPwdOnItsOwnLine(t *testing.T) {
+
+}
