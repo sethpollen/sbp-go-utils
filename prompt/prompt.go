ca12f0c6b612b6754f1e16d64de09af8b5d4754c
Seth Pollen <pollen@google.com>
2014-10-29 17:14:02 -0500

    MakePrompt implementation

--- prompt/prompt.go
+++ prompt/prompt.go
@@ -3,6 +3,7 @@
 package prompt
 
 import "fmt"
+import "math"
 import "os"
 import "os/exec"
 import "strings"
@@ -12,9 +13,9 @@ import "time"
 //   maxWidth - Maximum width that the prompt string may occupy, in characters.
 //   info - An "info" string, which appears next to the PWD.
 //   pwd - The current working directory to display.
-//   errorcode - The result code of the previous shell command.
+//   exitCode - The result code of the previous shell command.
 //   flag - A short "flag" string, which appears before the final $.
-func MakePrompt(maxWidth int, info string, pwd string, errorCode int,
+func MakePrompt(maxWidth int, info string, pwd string, exitCode int,
 	flag string) *Prompt {
 	// Perform tilde collapsing on the PWD.
 	var home = os.Getenv("HOME")
@@ -31,38 +32,81 @@ func MakePrompt(maxWidth int, info string, pwd string, errorCode int,
 	// Format the date and time.
 	var dateTime = time.Now().Format("01/02 15:04")
 
-	// Now we must do the negotiation to decide how many characters to give to
-	// each part of the prompt line. Start with non-negotiables.
+  // Construct the prompt text which must precede the PWD.
+  var promptBeforePwd Prompt
 
 	// Date and time.
-	var dateTimePrompt Prompt
-	dateTimePrompt.Style(Cyan, true)
-	dateTimePrompt.Write(dateTime + " ")
+	promptBeforePwd.Style(Cyan, true)
+	promptBeforePwd.Write(dateTime + " ")
 
 	// Hostname.
-	var hostnamePrompt Prompt
 	if runningOverSsh {
-		hostnamePrompt.Style(Yellow, false)
-		hostnamePrompt.Write("(")
+		promptBeforePwd.Style(Yellow, false)
+		promptBeforePwd.Write("(")
 	}
-	hostnamePrompt.Style(Magenta, true)
-	hostnamePrompt.Write(shortHostname)
+	promptBeforePwd.Style(Magenta, true)
+	promptBeforePwd.Write(shortHostname)
 	if runningOverSsh {
-		hostnamePrompt.Style(Yellow, false)
-		hostnamePrompt.Write(") ")
+		promptBeforePwd.Style(Yellow, false)
+		promptBeforePwd.Write(") ")
 	}
 
 	// Info (if we got one).
-	var infoPrompt Prompt
 	if len(info) > 0 {
-		infoPrompt.Style(White, false)
-		infoPrompt.Write("[")
-		infoPrompt.Style(White, true)
-		infoPrompt.Write(info)
-		infoPrompt.Style(White, false)
-		infoPrompt.Write("] ")
+		promptBeforePwd.Style(White, false)
+		promptBeforePwd.Write("[")
+		promptBeforePwd.Style(White, true)
+		promptBeforePwd.Write(info)
+		promptBeforePwd.Style(White, false)
+		promptBeforePwd.Write("] ")
 	}
 
-	var p = new(Prompt)
-	return p
+  // Construct the prompt text which must follow the PWD.
+  var promptAfterPwd Prompt
+
+  // Exit code.
+  if exitCode != 0 {
+    promptAfterPwd.Style(Red, true)
+    promptAfterPwd.Write(fmt.Sprintf("[%d]", exitCode)
+  }
+
+  // Determine how much space is left for the PWD.
+  var pwdWidth =
+    math.Max(0, maxWidth - promptBeforePwd.Len() - promptAfterPwd.Len())
+  var pwdOnItsOwnLine = false
+  if maxWidth >= 25 && pwdWidth < 25 {
+    // Don't cram the PWD into a tiny space; put it on its own line.
+    pwdWidth = maxWidth
+    pwdOnItsOwnLine = true
+  }
+
+  // Subtract 2 in case we have to include the ".." characters.
+  var pwdStart = len(pwd) - (pwdWidth - 2)
+  if start > 0 {
+    // Truncate the pwd.
+    if start >= len(pwd) {
+      // There is no room for the PWD at all.
+      pwd = ""
+    } else {
+      pwd = ".." + pwd[start:]
+    }
+  }
+
+  // Build the complete prompt string.
+  var fullPrompt Prompt
+  fullPrompt.Append(promptBeforePwd)
+  if pwdOnItsOwnLine {
+    fullPrompt.Append(promptAfterPwd)
+    fullPrompt.Write("\n")
+    fullPrompt.Style(Cyan, true)
+    fullPrompt.Write(pwd)
+  } else {
+    fullPrompt.Style(Cyan, true)
+    fullPrompt.Write(pwd + " ")
+    fullPrompt.Append(promptAfterPwd)
+  }
+  fullPrompt.Style(Yellow, true)
+  fullPrompt.Write("\n" + flag + "$ ")
+
+  return fullPrompt
 }
