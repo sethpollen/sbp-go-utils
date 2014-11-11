16e322b20a6000c7308d9ba9f8715a3878d2065e
Seth Pollen <pollen@google.com>
2014-11-11 17:32:49 -0600

    Tests now working :)

--- prompt/path_test.go
+++ prompt/path_test.go
@@ -3,12 +3,12 @@ package prompt
 import "testing"
 
 func TestEmpty(t *testing.T) {
-/*  var r = RelativePath("", "abc")
+  var r = RelativePath("", "abc")
   if r != "" {
 		t.Errorf("Expected \"\", got \"%s\"", r)
   }
   r = RelativePath("abc", "")
   if r != "abc" {
     t.Errorf("Expected \"abc\", got \"%s\"", r)
-  }*/
+  }
 }
