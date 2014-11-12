5d961b0667b74989ad7f89a26a560e04b6df4980
Seth Pollen <pollen@google.com>
2014-11-12 17:29:09 -0600

    log

--- prompt/main/main.go
+++ prompt/main/main.go
@@ -1,14 +1,12 @@
 package main
 
-import "fmt"
-import "os"
+import "log"
 import "code.google.com/p/sbp-go-utils/git"
 import "code.google.com/p/sbp-go-utils/prompt"
 
 func main() {
   err := prompt.DoMain([]prompt.PwdMatcher{git.GitMatcher})
   if err != nil {
-    fmt.Fprintln(os.Stderr, err)
-    os.Exit(1)
+    log.Fatalln(err)
   }
 }
