b86eb8149b6138b6bb0470c4e79b243fbd2a9e1e
Seth Pollen <pollen@google.com>
2014-11-15 03:32:23 -0600

    hg

---
+++ hg/hg.go
@@ -0,0 +1,52 @@
+// Utilities for dealing with Mercurial repositories. Mercurial is written in
+// Python, and as such it is pretty slow. It takes about 50 ms to run
+// "hg status" on a repo. This is too long to run inline in my command prompt.
+// So this file contains some hacks to query Mercurial repo information without
+// invoking any part of the Mercurial codebase.
+package hg
+
+import "errors"
+import "os"
+import "path"
+import "code.google.com/p/sbp-go-utils/prompt"
+import "code.google.com/p/sbp-go-utils/util"
+
+// Encapsulates information about an Hg repo.
+type HgInfo struct {
+  // Name of this Hg repo.
+  RepoName string
+  // Pwd, relative to the root repo path.
+  RelativePwd string
+}
+
+func GetHgInfo(pwd string) (*HgInfo, error) {
+  repoPath, err := util.SearchParents(pwd, isHgRepo)
+  if err != nil {
+    return nil, errors.New("Not in an Hg repo")
+  }
+  var info = new(HgInfo)
+  info.RepoName = path.Base(repoPath)
+  info.RelativePwd = util.RelativePath(pwd, repoPath)
+  return info
+}
+
+func isHgRepo(pwd string) bool {
+  var metaDir = path.Join(pwd, ".hg")
+  fileInfo, err := os.Stat(metaDir)
+  return err != nil && fileInfo.IsDir()
+}
+
+// A PwdMatcher that matches any directory inside an Hg repo.
+type HgMatcher {}
+
+func (self HgMatcher) Match(env *prompt.PromptEnv) bool {
+  hgInfo, err := GetHgInfo(env.Pwd)
+  if err != nil {
+    return false
+  }
+  env.Info = hgInfo.RepoName
+  env.Flag.Style(prompt.Magenta, false)
+  env.Flag.Write("hg")
+  env.Pwd = hgInfo.RelativePwd
+  return true
+}
