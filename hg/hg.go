e3c3575073296c4c03095337541d5c913d6240f1
Seth Pollen <pollen@google.com>
2014-11-17 10:23:51 -0600

    Caching for expensive hg info

--- hg/hg.go
+++ hg/hg.go
@@ -5,9 +5,11 @@
 // invoking any part of the Mercurial codebase.
 package hg
 
+import "encoding/json"
 import "errors"
 import "os"
 import "path"
+import "strings"
 import "code.google.com/p/sbp-go-utils/prompt"
 import "code.google.com/p/sbp-go-utils/util"
 
@@ -15,17 +17,29 @@ import "code.google.com/p/sbp-go-utils/util"
 type HgInfo struct {
 	// Name of this Hg repo.
 	RepoName string
+  // Full path to the root of this repo.
+  RepoPath string
 	// Pwd, relative to the root repo path.
 	RelativePwd string
   // True if there are uncommitted local changes.
   Dirty bool
 }
 
+// Encapsulates information about an Hg repo which is expensive to compute.
+type ExpensiveHgInfo struct {
+  // True if there are unpushed local commits.
+  Unpushed bool
+}
+
+func (self *HgInfo) key() string {
+  return self.RepoPath
+}
+
 func GetHgInfo(pwd string) (*HgInfo, error) {
-	repoPath, err := util.SearchParents(pwd, isHgRepoRoot)
-	if err != nil {
-		return nil, errors.New("Not in an Hg repo")
-	}
+	repoPath, err := getHgRepoRoot(pwd)
+  if err != nil {
+    return nil, err
+  }
 
   // Now that we know we are in an Hg repo, it's worth paying the cost to run
   // hg status.
@@ -36,9 +50,18 @@ func GetHgInfo(pwd string) (*HgInfo, error) {
 
 	var info = new(HgInfo)
 	info.RepoName = path.Base(repoPath)
+  info.RepoPath = repoPath
 	info.RelativePwd = util.RelativePath(pwd, repoPath)
   info.Dirty = (status != "")
 	return info, nil
+}
+
+func getHgRepoRoot(pwd string) (string, error) {
+	repoPath, err := util.SearchParents(pwd, isHgRepoRoot)
+	if err != nil {
+		return "", errors.New("Not in an Hg repo")
+	}
+  return repoPath, nil
 }
 
 func isHgRepoRoot(pwd string) bool {
@@ -47,25 +70,74 @@ func isHgRepoRoot(pwd string) bool {
 	return err == nil && fileInfo.IsDir()
 }
 
+func GetExpensiveHgInfo(pwd string) (*ExpensiveHgInfo, error) {
+  // This command takes a while because it actually contacts the remote server.
+  outgoing, err := util.EvalCommandSync(pwd, "hg", "outgoing", "--limit=1")
+  if err != nil {
+    return nil, err
+  }
+
+  var info = new(ExpensiveHgInfo)
+  info.Unpushed = !strings.Contains(outgoing, "no changes found")
+  return info, nil
+}
+
 // A prompt.Module that matches any directory inside an Hg repo.
 type module struct{}
 
 func (self module) Prepare(env *prompt.PromptEnv) {}
 
-// TODO: Cache results of 'hg outgoing'
 func (self module) Match(env *prompt.PromptEnv, updateCache bool) bool {
 	hgInfo, err := GetHgInfo(env.Pwd)
 	if err != nil {
 		return false
 	}
+
+  var expensiveInfo *ExpensiveHgInfo = nil
+  if updateCache {
+    expensiveInfo, err = GetExpensiveHgInfo(env.Pwd)
+    if err == nil {
+      value, err := json.Marshal(expensiveInfo)
+      if err == nil {
+        var item memcache.Item
+        item.Key = hgInfo.key()
+        item.Value = value
+        env.Memcache.Set(&item)
+      }
+    }
+  } else {
+    // Just try to read the cache.
+    expensiveInfo, _ = readCachedInfo(hgInfo, env.Memcache)
+  }
+
 	env.Info = hgInfo.RepoName
-  if hgInfo.Dirty {
-    env.Info += " *"
+  if hgInfo.Dirty || expensiveInfo.Unpushed {
+    env.Info += " "
+    if expensiveInfo.Unpushed {
+      env.Info += "^"
+    }
+    if hgInfo.Dirty {
+      env.Info += "*"
+    }
   }
 	env.Flag.Style(prompt.Magenta, true)
 	env.Flag.Write("hg")
 	env.Pwd = hgInfo.RelativePwd
 	return true
+}
+
+func readCachedInfo(hgInfo *HgInfo, mc *memcache.Client) (
+    *ExpensiveHgInfo, error) {
+  item, err := mc.Get(hgInfo.key())
+  if err != nil {
+    return nil, err
+  }
+  var expensiveInfo = new(ExpensiveHgInfo)
+  err = json.Unmarshal(item.Value, expensiveInfo)
+  if err != nil {
+    return nil, err
+  }
+  return expensiveInfo, nil
 }
 
 func (self module) Description() string {
